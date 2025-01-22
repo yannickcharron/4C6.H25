861cd80979052cf6e203cf08b407f2a17ab1edd6
Yannick Charron <ycharron@cstj.qc.ca>
2025-01-22 10:30:59 -0500

    feature: Main UI

--- S01.PremiereApplication/app/src/main/java/ca/qc/cstj/firstapplication/MainActivity.kt
+++ S01.PremiereApplication/app/src/main/java/ca/qc/cstj/firstapplication/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ca.qc.cstj.firstapplication.ui.theme.FirstApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirstApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Yannick",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Text(text = 0.toString(), fontSize = 32.sp)
+                        Column {
+                            Button(onClick = {}) {
+                                Text(text = "+", fontSize = 20.sp)
+                            }
+                            Button(onClick = {}) {
+                                Text(text = "-", fontSize = 20.sp)
+                            }
+                        }
+                    }
+                    Button(
+                        modifier = Modifier.fillMaxWidth(0.5f),
+                        onClick = { }
+                    ) {
+                        Text(text = "Try", fontSize = 32.sp)
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstApplicationTheme {
-        Greeting("Android")
-    }
-}
